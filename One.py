35c859115c9908b240152de53c490ab8e2e244e6
Altefacts <artefacts@github.com>
2013-04-10 15:59:30 -0400

    Scammed.

--- One.py
+++
@@ -1,7 +0,0 @@
-#1. There are a couple inefficiencies with this approach:
-#- first, the use of variables sum, and i are unnecessary.
-#- repeated calls to the user-level + operator are also not optimal
-#- even the print statement is unnecessary 
-# Using the built-in 'sum(range(0,1000000))' eliminates these.
-
-print sum(range(0,1000000));
--- Two.py
+++
@@ -1,9 +0,0 @@
-a =[5,4,3,3,4,2,1];
-print a;
-b =[4,3,2,3,2,1,5];
-print b;
-a =list(set(a));
-print a;
-b =list(set(b));
-print b;
-print [i+j for i,j in zip(a,b)]
